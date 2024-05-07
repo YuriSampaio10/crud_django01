ae61e07b6250b32b465df55825e28229ef08fd78
Yuri Sampaio <ynogueira317@gmail.com>
2024-05-06 21:20:05 -0300

    arrumando o app medicos

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+from core import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("medico.urls")),
-]
+    path('', include('medicos.urls')),  # Inclui as URLs do seu aplicativo
+]  
--- medicos/apps.py
+++ medicos/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MedicoConfig(AppConfig):
+class MedicosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'medico'
+    name = 'medicos'
