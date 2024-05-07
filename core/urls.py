fe21e46a7aee9650600eddcea24cfdd77bf449e8
Yuri Sampaio <ynogueira317@gmail.com>
2024-05-06 21:56:04 -0300

    templates do crud

--- core/urls.py
+++ core/urls.py
@@ -16,11 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-
-from core import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('medicos.urls')),  # Inclui as URLs do seu aplicativo
-]  
+    path('', include('medico.urls')) # inclu as URLS do app Medico
+]
